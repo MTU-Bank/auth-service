13f7cf1a13ac8793f8db6764be8b7d14b901c39c
vest1ter <geninkirill526@gmail.com>
2024-03-01 21:04:00 +0300

    Добавление базовой логики работы системы авторизации пользователя

---
+++ ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTUBankBase.Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTUAuthService
+{
+    internal class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                Program.connectionString,
+                ServerVersion.AutoDetect(Program.connectionString)
+            );
+        }
+
+        public DbSet<User> Users { get; set; }
+        ы
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,54 @@
-﻿using MTUAuthService.ServiceUtils;
+﻿using EmbedIO.Routing;
+using EmbedIO.WebApi;
+using EmbedIO;
+using MTUAuthService.ServiceUtils;
+using MTUBankBase.Auth.Models;
 using MTUBankBase.Config;
 using MTUBankBase.Helpers;
 
 namespace MTUAuthService
 {
+    public class UserManager
+    {
+        public static void Add(RegisterRequest user)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+
+
+                db.UsersTable.Add(user);
+                db.SaveChanges();
+
+            }
+        }
+
+        public static AuthResult GetUser(string EntUserPhone)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var res = db.UsersTabel.FirstOrDefault(res => res.phone_number == EntUserPhone);
+                Verification.TwoFactorAuth.Enable2FA(res.username);
+                return res;
+            }
+
+        }
+
+    }
+    /*
+    
+    public class Checker
+    {
+        public static AuthResult Check(TwoFARequest TwoFACode)
+        {
+            
+            var result = new AuthResult() { };
+            return Verification.TwoFactorAuth.VerifyOTP();
+        }
+    }
+    
+    [Table("users")]
+    */
+
     internal class Program
     {
         public static ServiceConfig serviceConfig;
@@ -20,6 +65,29 @@ namespace MTUAuthService
             await ServiceInitializer.BindServiceAsync();
 
             await Task.Delay(-1); // we are done here. Go to Controller for Web API methods.
+        }
+
+        [Route(HttpVerbs.Post, "/api/registerUser")]
+        public async Task<AuthResult> DescribePerson([JsonData] RegisterRequest user)
+        {
+            UserManager.Add(user);
+
+            return user;
+        }
+
+        [Route(HttpVerbs.Post, "/api/loginUser")]
+        public async Task<AuthResult> LoginPerson([JsonData] AuthRequest EntUserPhone)
+        {
+
+            return UserManager.GetUser(EntUserPhone);
+        }
+
+        [Route(HttpVerbs.Post, "/api/2FA")]
+        public async Task<bool> TwoFAVerification([JsonData] TwoFARequest TwoFACode)
+        {
+            string x = TwoFACode.TwoFAToken;
+            string y = TwoFACode.TwoFAValue;
+            return Verification.TwoFactorAuth.VerifyOTP(username: x, userEnteredOTP: y);
         }
     }
 }
